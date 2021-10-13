f55819508092071063ab2b3807a335d45d1c81ad
genelorenz0408 <sarmientoemmener83@gmail.com>
2021-10-13 18:24:04 +0800

    Added NavBar

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,20 @@
-import React, { Component } from 'react';
-
+import React, { Component } from "react";
+import { BrowserRouter, Switch, Route, Link, NavLink } from "react-router-dom";
+import Dashboard from "./Dashboard";
+import Home from "./Home";
+import Navigation from "./Navigation";
 export default class App extends Component {
   render() {
     return (
-      <div className='app'>
-        <h1>DevCamp React Starter</h1>
-        <h2>React Redux Router</h2>
+      <div className="app">
+        <BrowserRouter>
+          <Navigation />
+          <Switch>
+            <Route exact path={"/"} component={Home} />
+
+            <Route exact path={"/dashboard"} component={Dashboard} />
+          </Switch>
+        </BrowserRouter>
       </div>
     );
   }
